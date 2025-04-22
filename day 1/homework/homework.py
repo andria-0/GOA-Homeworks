697c792da69cd7b4c58c115b3653b754d172d6e5
andria-0 <andriasologashvili@gmail.com>
2025-04-22 19:46:03 +0400

    second commit

--- day 1/homework/homework.py
+++ day 1/homework/homework.py
@@ -4,4 +4,4 @@ hight=160.1
 live="mirian mefes qucha 100"
 surname="sologashvili"
 
-print("my name is " + name+  surname+ "i am"+ (str(age))+ "i live in"+ live + "in hight i am" + (str(hight)))
+print("my name is " + name + surname+ " i am "+ (str(age))+ " i live in "+ live + " in hight i am " + (str(hight)))
